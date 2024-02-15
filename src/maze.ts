d77005d40c2eb53fa03985239d271e2bf5aed798
sisu6828 <148442550+sisu6828@users.noreply.github.com>
2024-02-15 15:12:05 +0100

    Updated TODO

--- src/maze.ts
+++ src/maze.ts
@@ -1,12 +1,23 @@
+
+
 type free = true;
+
 type wall = false;
 
 type maze_block = wall | free;
 
-// TODO fixed size tuple of generic length
+
+// TODO fixed size tuple of generic length try to incorperate maze size into the type
+
 type maze = {
     width: number,
     height: number,
     matrix: Array<Array<maze_block>>;
 
 }
+
+// export function view_maze(inp:maze) {
+
+//     while()
+
+// }
