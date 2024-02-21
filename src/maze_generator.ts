45f98453f051715fcb76351cf67f7e447442da15
Simon <148442550+sisu6828@users.noreply.github.com>
2024-02-21 12:34:53 +0100

    added a test file and comment out some stuff to make tsc stop whining

--- src/maze_generator.ts
+++ src/maze_generator.ts
@@ -1,27 +1,29 @@
-import { Maze, MazeBlock } from "./maze";
+import { Maze, new_maze } from "./maze";
 
 // An algorithm that uses the recursive division method specified here
 // https://en.wikipedia.org/wiki/Maze_generation_algorithm#Recursive_division_method
+// Work in progress...
+export function generate_maze(side: number, seed?: number): Maze {
 
-function generate_maze(w: number, h: number, seed?: number): Maze {
-    function seeded_random(input: number): [number, number] {
-        return [1, 2];
-    }
+    let maze = new_maze(side);
 
+
+    // function seeded_random(input: number): [number, number] {
+    //     return [1, 2];
+    // }
     if (seed === undefined) {
-        return {
-            width: w,
-            height: h,
-            matrix: [[true]]
-        };
-    } else {
-        const seeded_value = seeded_random(seed);
+        return maze;
+        
 
-        return {
-            width: w,
-            height: h,
-            matrix: [[true]]
-        };
+    } else {
+        // const seeded_value = seeded_random(seed);
+
+        return maze;
     }
 }
 
+
+// pick a number in between 0 to width-1 and draw a line of blocked 
+// in that path across the entire maze
+
+// 
