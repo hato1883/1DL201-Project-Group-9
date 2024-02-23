7507996d1b5e863f8ab5b69119baedfa78ae9482
Simon <148442550+sisu6828@users.noreply.github.com>
2024-02-23 08:06:42 +0100

    added some tests

--- tests/maze_generator.test.ts
+++ tests/maze_generator.test.ts
@@ -11,3 +11,25 @@ test('Is there a solvable 1x1 maze returned?', () => {
     } as Maze
     )
 });
+
+test('Is there a solvable 2x2 maze returned?', () => {
+    expect(generate_maze(2)).toStrictEqual(
+    {
+        width: 2,
+        height: 2,
+            matrix: [[free, free],
+                     [free, free]]
+    } as Maze
+    )
+});
+
+test('Is there a solvable 3x3 maze returned?', () => {
+    expect(generate_maze(3)).toStrictEqual(
+    {
+        width: 3,
+        height: 3,
+            matrix: [[free, free, free],
+                     [free, free, free]]
+    } as Maze
+    )
+});
