4986c8a734cbf0858b198f486e50e534aee8d4a8
Hampus Toft <hampus.toft.1883@student.uu.se>
2024-02-23 13:13:56 +0100

    Some progress, still not working tho.

--- tests/maze_generator.test.ts
+++ tests/maze_generator.test.ts
@@ -1,35 +1,35 @@
-import { generate_maze } from "../src/maze_generator";
-import { Maze,free } from "../src/maze";
+import { iterative_maze_generation } from "../src/maze_generator";
+import { Maze, free } from "../src/maze";
 
 
-test('Is there a solvable 1x1 maze returned?', () => {
-    expect(generate_maze(1)).toStrictEqual(
-    {
-        width: 1,
-        height: 1,
-        matrix: [[free]]
-    } as Maze
-    )
+test("Is there a solvable 1x1 maze returned?", () => {
+    expect(iterative_maze_generation(1)).toStrictEqual(
+        {
+            width: 1,
+            height: 1,
+            matrix: [[free]]
+        } as Maze
+    );
 });
 
-test('Is there a solvable 2x2 maze returned?', () => {
-    expect(generate_maze(2)).toStrictEqual(
-    {
-        width: 2,
-        height: 2,
+test("Is there a solvable 2x2 maze returned?", () => {
+    expect(iterative_maze_generation(2)).toStrictEqual(
+        {
+            width: 2,
+            height: 2,
             matrix: [[free, free],
                      [free, free]]
-    } as Maze
-    )
+        } as Maze
+    );
 });
 
-test('Is there a solvable 3x3 maze returned?', () => {
-    expect(generate_maze(3)).toStrictEqual(
-    {
-        width: 3,
-        height: 3,
+test("Is there a solvable 3x3 maze returned?", () => {
+    expect(iterative_maze_generation(3)).toStrictEqual(
+        {
+            width: 3,
+            height: 3,
             matrix: [[free, free, free],
                      [free, free, free]]
-    } as Maze
-    )
+        } as Maze
+    );
 });
