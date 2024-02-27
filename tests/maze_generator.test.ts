cd9a179fda3b7219da688b89aaf64bb964dd1e80
Hampus Toft <hampus.toft.1883@student.uu.se>
2024-02-27 21:19:04 +0100

    Fixed maze generation tests.

--- tests/maze_generator.test.ts
+++ tests/maze_generator.test.ts
@@ -3,7 +3,7 @@ import { Maze, free, wall } from "../src/maze";
 
 
 test("Is there a solvable 1x1 maze returned?", () => {
-    expect(iterative_maze_generation(1)).toStrictEqual(
+    expect(iterative_maze_generation(1, 0, false)).toStrictEqual(
         {
             width: 3,
             height: 3,
@@ -17,14 +17,14 @@ test("Is there a solvable 1x1 maze returned?", () => {
 });
 
 test("Is there a solvable 2x2 maze returned?", () => {
-    expect(iterative_maze_generation(2)).toStrictEqual(
+    expect(iterative_maze_generation(2, 0, false)).toStrictEqual(
         {
             width: 5,
             height: 5,
             matrix: [
                 [wall, wall, wall, wall, wall],
-                [wall, free, free, free, wall],
-                [wall, wall, wall, free, wall],
+                [wall, free, wall, free, wall],
+                [wall, free, wall, free, wall],
                 [wall, free, free, free, wall],
                 [wall, wall, wall, wall, wall]
             ]
@@ -33,15 +33,15 @@ test("Is there a solvable 2x2 maze returned?", () => {
 });
 
 test("Is there a solvable 3x3 maze returned?", () => {
-    expect(iterative_maze_generation(3)).toStrictEqual(
+    expect(iterative_maze_generation(3, 0, false)).toStrictEqual(
         {
             width: 7,
             height: 7,
             matrix: [
                 [wall, wall, wall, wall, wall, wall, wall],
-                [wall, free, free, free, wall, free, wall],
-                [wall, free, wall, wall, wall, free, wall],
-                [wall, free, wall, free, free, free, wall],
+                [wall, free, free, free, free, free, wall],
+                [wall, wall, wall, free, wall, free, wall],
+                [wall, free, wall, free, wall, free, wall],
                 [wall, free, wall, wall, wall, free, wall],
                 [wall, free, free, free, free, free, wall],
                 [wall, wall, wall, wall, wall, wall, wall]
